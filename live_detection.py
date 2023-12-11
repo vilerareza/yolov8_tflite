816245c112045ad6e833afb06c3e7dc0641106ad
Reza Vilera <vilera_reza99@yahoo.com>
2023-12-11 15:18:09 +0700

    first commit

--- live_detection.py
+++ live_detection.py
@@ -71,9 +71,10 @@ def start_live_detection(cam,
             detector.set_tensor(detector_input['index'], frame)
             detector.invoke()
             # Bounding boxes coordinates
-            output = detector_output[1]# ['index']
-            print (output)
-            #bboxes = detector.get_tensor(detector_output[1]['index'])[0]
+            # output = detector_output[1]# ['index']
+            # print (output)
+            bboxes = detector.get_tensor(detector_output[0]['index'])
+            print (bboxes)
             # Detected objects class ID
             #class_ids = detector.get_tensor(detector_output[3]['index'])[0]
             # Detection scores
