e2b54d0be174ec641e738d5f1b26f5bc49c04106
Reza Vilera <vilera_reza99@yahoo.com>
2023-12-11 20:46:38 +0700

    first commit

--- live_detection.py
+++ live_detection.py
@@ -97,7 +97,7 @@ class YoloV8Detector:
             # Resize the frame to match the model input size
             frame = cv.resize(frame, input_size).astype('int8')
             frame = cv.normalize(frame, None, 0, 127, cv.NORM_MINMAX, dtype=cv.CV_8S)
-            frame = frame[:,:,::-1]
+            #frame = frame[:,:,::-1]
             frame = np.expand_dims(frame, axis=0)
 
             # ''' Run object detection '''
